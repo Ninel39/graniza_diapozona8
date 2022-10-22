00ee774d8155b8db96bcffa935f7483b5fa9c398
User <User@WIN-5U3F60ENH1L>
2022-10-23 02:11:24 +0300

    Добавьте файлы проекта.

---
+++ graniza_diapozona8/graniza_diapozona8.cpp
@@ -0,0 +1,21 @@
+﻿// graniza_diapozona8.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    int a, b, sum = 0;
+    cout << "ВВедите первое число""\n";
+    cin >> a;
+    cout << "ВВедите второе число""\n";
+    cin >> b;
+
+    for (int i = a; i <= b; ++i)    sum += i;
+    {
+        cout << sum << endl;
+    }
+}
+
